d65ea0dc12f3fd74ffc69cdd10abbda69474edd3
Annie78724 <ananya2003pal@gmail.com>
2024-12-20 16:22:17 +0530

    Added components

---
+++ src/app/home/flight-info/flight-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './flight-info.component.html',
+  styleUrl: './flight-info.component.scss'
+})
+export class FlightInfoComponent {
+
+}
